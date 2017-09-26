183a800ee2624ffaca8f2e90ba533bf5c5ca7279
April Eggers <april.eggers@flatironsjouve.com>
2017-09-25 22:06:57 -0600

    First (silly) attempt at reading JSON and transforming to xml. Something isn't quite right because the number of ISO processes isn't going up

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ module.exports = app;
 // and dump it to the client.
     var headers = {
         destination: '/queue/test',
-        ack: 'client',
-        'activemq.prefetchSize': '1'
+        ack: 'client'/*,
+        'activemq.prefetchSize': '1'*/
     };
 
     client.connect();
@@ -48,6 +48,75 @@ module.exports = app;
     client.on('message', function(message) {
         console.log("Got message: " + message.headers['message-id']);
         console.log(message.body);
+        var parsedJson = JSON.parse(message.body[0]);
+        console.log(parsedJson.BatchId);
+        var xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:case=\"http://bpms.everteam.com/Processes/Core/CaseManagement/Case_Manager\" xmlns:laun=\"http://www.example.org/Launchpoint\">" +
+            "<soapenv:Header/>" +
+            "        <soapenv:Body>" +
+            "        <case:Read_case_batchRequest>" +
+            "<laun:BatchId>" + parsedJson.BatchId + "</laun:BatchId>\n" +
+            "         <laun:EnvironmentId>" + parsedJson.EnvironmentId + "</laun:EnvironmentId>\n" +
+            "         <laun:ClientId>" + parsedJson.ClientId + "</laun:ClientId>\n" +
+            "         <laun:TotalCaseCount>" + parsedJson.TotalCaseCount + "</laun:TotalCaseCount>\n";
+
+       for (var i = 0; i < parsedJson.Cases; i++) {
+           var claimCase = parsedJson[i];
+            xml += "         <laun:Cases>\n" +
+                "            <laun:CaseId>"+claimCase.CaseId+"</laun:CaseId>\n" +
+                "            <laun:ISOIndicator>"+claimCase.ISOIndicator+"</laun:ISOIndicator>\n" +
+                "            <laun:Score>" + claimCase.Score + "</laun:Score>\n" +
+                "            <laun:AccidentDate>" + claimCase.AccidentDate + "</laun:AccidentDate>\n" +
+                "            <laun:BenefitAmount>" + claimCase.BenefitAmount + "</laun:BenefitAmount>\n" +
+                "            <laun:FundingSource>" + claimCase.FundingSource + "</laun:FundingSource>\n" +
+                "            <laun:LOB>" + claimCase.LOB + "</laun:LOB>\n" +
+                "            <laun:State>" + claimCase.State + "</laun:State>\n" +
+                "            <laun:WorkComp>" + claimCase.WorkComp + "</laun:WorkComp>\n" +
+                "            <laun:CaseStatus>" + claimCase.CaseStatus + "</laun:CaseStatus>\n" +
+                "            <laun:CaseSource>" + claimCase.CaseSource + "</laun:CaseSource>\n" +
+                "         </laun:Cases>\n";
+       }
+
+        xml += "      </case:Read_case_batchRequest>\n" +
+        "   </soapenv:Body>\n" +
+        "</soapenv:Envelope>";
+
+
+
+
+        client.send({
+            'destination': '/queue/LaunchPointProcess_Processes_Core_CaseManagement_Case_Manager_Queue_Service',
+            'body': xml,
+            'persistent': 'true'
+        });
+
+        /*        "         <!--1 or more repetitions:-->\n" +
+                    "         <laun:Cases>\n" +
+                    "            <laun:CaseId>456</laun:CaseId>\n" +
+                    "            <laun:ISOIndicator>1</laun:ISOIndicator>\n" +
+                    "            <laun:Score>67</laun:Score>\n" +
+                    "            <laun:AccidentDate>2017-05-03</laun:AccidentDate>\n" +
+                    "            <laun:BenefitAmount>5000</laun:BenefitAmount>\n" +
+                    "            <laun:FundingSource>Medicaid</laun:FundingSource>\n" +
+                    "            <laun:LOB>Health</laun:LOB>\n" +
+                    "            <laun:State>CO</laun:State>\n" +
+                    "            <laun:WorkComp>No</laun:WorkComp>\n" +
+                    "            <laun:CaseStatus>Investigate</laun:CaseStatus>\n" +
+                    "            <laun:CaseSource>Generated</laun:CaseSource>\n" +
+                    "         </laun:Cases>\n" +
+                    "         <laun:Cases>\n" +
+                    "            <laun:CaseId>456</laun:CaseId>\n" +
+                    "            <laun:ISOIndicator>1</laun:ISOIndicator>\n" +
+                    "            <laun:Score>67</laun:Score>\n" +
+                    "            <laun:AccidentDate>2017-05-03</laun:AccidentDate>\n" +
+                    "            <laun:BenefitAmount>5000</laun:BenefitAmount>\n" +
+                    "            <laun:FundingSource>Medicaid</laun:FundingSource>\n" +
+                    "            <laun:LOB>Health</laun:LOB>\n" +
+                    "            <laun:State>CO</laun:State>\n" +
+                    "            <laun:WorkComp>No</laun:WorkComp>\n" +
+                    "            <laun:CaseStatus>Investigate</laun:CaseStatus>\n" +
+                    "            <laun:CaseSource>Generated</laun:CaseSource>\n" +
+                    "         </laun:Cases>\n" +*/
+
         client.ack(message.headers['message-id']);
     });
 
