708a5e9db7340669e674bb9d0960da5ad56daa88
April Eggers <april.eggers@flatironsjouve.com>
2017-09-26 15:11:59 -0600

    Refactored the json to xml converter to be more generic. Replaced parseBatch with parseObject and replaced parseCases with parseArray Created a generic method that constructs xml elements

--- util/jsonConverter.js
+++ util/jsonConverter.js
@@ -13,6 +13,16 @@ var soapEnvelopeFooter = "" +
     "   </soapenv:Body>\n" +
     "</soapenv:Envelope>";
 
+var createXMLElement = function (namespace, elementName, elementValue, createEndTag) {
+    var xml = "";
+    if (elementValue === null || typeof elementValue === "undefined") {
+        xml = createEndTag ? "</" : "<";
+        xml += namespace + ":" + elementName + ">\n";
+    } else {
+        xml = "<" + namespace + ":" + elementName + ">" + elementValue + "</" + namespace + ":" + elementName + ">\n";
+    }
+    return xml;
+};
 
 var convertJsonStringToObject = function (jsonString) {
     var jsonObject;
@@ -24,57 +34,38 @@ var convertJsonStringToObject = function (jsonString) {
     return jsonObject;
 };
 
-var parseBatch = function (json) {
-/*    "<laun:BatchId>" + parsedJson.BatchId + "</laun:BatchId>\n" +
-    "         <laun:EnvironmentId>" + parsedJson.EnvironmentId + "</laun:EnvironmentId>\n" +
-    "         <laun:ClientId>" + parsedJson.ClientId + "</laun:ClientId>\n" +
-    "         <laun:TotalCaseCount>" + parsedJson.TotalCaseCount + "</laun:TotalCaseCount>\n";*/
-    var batch = "";
+var parseObject = function (json) {
+    var xml = "";
     for (var key in json) {
-        if (!Array.isArray(json[key])) {
-            batch += "<laun:" + key + ">" + json[key] + "</laun:"+key+">\n";
+        if (Array.isArray(json[key])) {
+            xml += parseArray(key, json[key]);
+        } else {
+            xml += createXMLElement("laun", key, json[key]);
         }
     }
-    console.log("The batch is " + batch);
-    return batch;
+    console.log("The translated json is " + xml);
+    return xml;
 };
 
-var parseCases = function (json) {
+var parseArray = function (arrayName, json) {
 
-/*for (var i = 0; i < parsedJson.Cases.length; i++) {
-           var claimCase = parsedJson.Cases[i];
-            xml += "         <laun:Cases>\n" +
-                "            <laun:CaseId>"+claimCase.CaseId+"</laun:CaseId>\n" +
-                "            <laun:ISOIndicator>"+claimCase.ISOIndicator+"</laun:ISOIndicator>\n" +
-                "            <laun:Score>" + claimCase.Score + "</laun:Score>\n" +
-                "            <laun:AccidentDate>" + claimCase.AccidentDate + "</laun:AccidentDate>\n" +
-                "            <laun:BenefitAmount>" + claimCase.BenefitAmount + "</laun:BenefitAmount>\n" +
-                "            <laun:FundingSource>" + claimCase.FundingSource + "</laun:FundingSource>\n" +
-                "            <laun:LOB>" + claimCase.LOB + "</laun:LOB>\n" +
-                "            <laun:State>" + claimCase.State + "</laun:State>\n" +
-                "            <laun:WorkComp>" + claimCase.WorkComp + "</laun:WorkComp>\n" +
-                "            <laun:CaseStatus>" + claimCase.CaseStatus + "</laun:CaseStatus>\n" +
-                "            <laun:CaseSource>" + claimCase.CaseSource + "</laun:CaseSource>\n" +
-                "         </laun:Cases>\n";
-       }*/
-    var cases = "";
-    for (var i = 0; i < json.Cases.length; i++) {
-        var claimCase = json.Cases[i];
-        cases += "<laun:Cases>"
-        for (var key in claimCase) {
-            cases += "<laun:" + key + ">" + claimCase[key] + "</laun:" + key + ">\n";
+    var xml = "";
+    for (var i = 0; i < json.length; i++) {
+        var object = json[i];
+        xml += createXMLElement("laun", arrayName, null, false);
+        for (var key in object) {
+            xml += createXMLElement("laun", key, object[key]);
         }
-        cases += "</laun:Cases>\n"
+        xml += createXMLElement("laun", arrayName, null, true);
     }
-    console.log("The cases are " + cases);
-    return cases;
+    console.log("The translated array xml is " + xml);
+    return xml;
 };
 
-var constructXmlMessage = function (batch, cases) {
+var constructXmlMessage = function (xmlBody) {
     return soapEnvelopeHeader +
         requestStartTag +
-        batch +
-        cases +
+        xmlBody +
         requestEndTag +
         soapEnvelopeFooter;
 }
@@ -85,9 +76,10 @@ module.exports =
         convertJSONMessageToXMl : function (jsonMessage) {
             var json = convertJsonStringToObject(jsonMessage);
             if (!json) return "";
-            var batch = parseBatch(json);
-            var cases = parseCases(json);
-            return constructXmlMessage(batch, cases);
+
+            var xml = parseObject(json);
+
+            return constructXmlMessage(xml);
         }
 
     };
