41cda93faf26743c7cfe73180b1fe0e93cc2ffe4
April Eggers <april.eggers@flatironsjouve.com>
2017-09-27 09:58:06 -0600

    Refactored the converter to use configured namespaces instead of hardcoded namespaces. Added function comments. Re-added defaults to the config file

--- config.js
+++ config.js
@@ -3,21 +3,15 @@ var config = {};
 config.active_mq = {};
 config.everteam = {};
 
-/*config.active_mq.host = process.env.ACTIVEMQ_HOST || 'localhost';
+config.active_mq.host = process.env.ACTIVEMQ_HOST || 'localhost';
 config.active_mq.port = process.env.ACTIVEMQ_PORT || 61613;
 config.active_mq.user = process.env.ACTIVEMQ_USER || 'admin';
 config.active_mq.password=  process.env.ACTIVEMQ_PASSWORD || 'admin';
 config.active_mq.source_queue = process.env.ACTIVEMQ_SOURCE_QUEUE || '/queue/test';
-config.active_mq.target_queue = process.env.ACTIVEMQ_TARGET_QUEUE || '/queue/test2';*/
-
-config.active_mq.host = process.env.ACTIVEMQ_HOST;
-config.active_mq.port = process.env.ACTIVEMQ_PORT;
-config.active_mq.user = process.env.ACTIVEMQ_USER;
-config.active_mq.password=  process.env.ACTIVEMQ_PASSWORD;
-config.active_mq.source_queue = process.env.ACTIVEMQ_SOURCE_QUEUE;
-config.active_mq.target_queue = process.env.ACTIVEMQ_TARGET_QUEUE;
+config.active_mq.target_queue = process.env.ACTIVEMQ_TARGET_QUEUE || '/queue/test2';
 
 config.everteam.process_namespace = "http://bpms.everteam.com/Processes/Core/CaseManagement/Case_Manager";
 config.everteam.schema_namespace = "http://www.example.org/Launchpoint";
+config.everteam.process_request="Read_case_batchRequest";
 
 module.exports = config;
--- util/jsonConverter.js
+++ util/jsonConverter.js
@@ -1,20 +1,31 @@
 var config = require("../config.js");
 
+/**
+ * Everteam generates namespace prefixes by lowercasing the first four letters of the last path element
+ * Duplicate that logic, so that namespaces can be computed instead of hardcoded
+ * @param namespace
+ * @returns {string}
+ */
+var createSchemaPrefix = function (namespace) {
+    var lastPathElemStart = namespace.lastIndexOf("/");
+    var lastPathElem =  namespace.substr(lastPathElemStart + 1, 4);
+    return lastPathElem.toLowerCase();
+};
 
-var soapEnvelopeHeader = "<soapenv:Envelope" +
-    " xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"" +
-    " xmlns:case=\"http://bpms.everteam.com/Processes/Core/CaseManagement/Case_Manager\"" +
-    " xmlns:laun=\"http://www.example.org/Launchpoint\">" +
-    "<soapenv:Header/>\n" +
-    "    <soapenv:Body>\n";
+// Used when referencing schema elements; i.e, <laun:batchId>
+var schemaNamespacePrefix = createSchemaPrefix(config.everteam.schema_namespace);
+// Used when referencing process elements, such as the name of the message event
+var processNamespacePrefix = createSchemaPrefix(config.everteam.process_namespace);
 
-var requestStartTag = "<case:Read_case_batchRequest>\n";
-var requestEndTag = "</case:Read_case_batchRequest>\n";
-
-var soapEnvelopeFooter = "" +
-    "   </soapenv:Body>\n" +
-    "</soapenv:Envelope>";
-
+/**
+ * A helper method to assemble an xml element string
+ * @param namespace
+ * @param elementName
+ * @param elementValue - if null, a start or end tag is created based on the value of createEndTag. If not null, both a
+ *                       beginning and end tag will be created.
+ * @param createEndTag - if true, creates only an end tag. If false, creates a beginning tag
+ * @returns {string} - The xml fragment
+ */
 var createXMLElement = function (namespace, elementName, elementValue, createEndTag) {
     var xml = "";
     if (elementValue === null || typeof elementValue === "undefined") {
@@ -26,6 +37,11 @@ var createXMLElement = function (namespace, elementName, elementValue, createEnd
     return xml;
 };
 
+/**
+ * Tries to parse the given string as JSON. If parsing fails, prints to the error log and returns an undefined object
+ * @param jsonString
+ * @returns {*}
+ */
 var convertJsonStringToObject = function (jsonString) {
     var jsonObject;
     try {
@@ -37,32 +53,62 @@ var convertJsonStringToObject = function (jsonString) {
     return jsonObject;
 };
 
+/**
+ * Takes a json object and outputs the equivalent XML. Does NOT handle nested objects
+ * @param json
+ * @returns {string}
+ */
 var parseObject = function (json) {
     var xml = "";
     for (var key in json) {
         if (Array.isArray(json[key])) {
             xml += parseArray(key, json[key]);
         } else {
-            xml += createXMLElement("laun", key, json[key]);
+            xml += createXMLElement(schemaNamespacePrefix, key, json[key]);
         }
     }
     console.log("The translated json is " + xml);
     return xml;
 };
 
+/**
+ * Takes an array of JSON objects and converts them to the equivalent XML
+ * @param arrayName
+ * @param json
+ * @returns {string}
+ */
 var parseArray = function (arrayName, json) {
     var xml = "";
     for (var i = 0; i < json.length; i++) {
         var object = json[i];
-        xml += createXMLElement("laun", arrayName, null, false);
+        xml += createXMLElement(schemaNamespacePrefix, arrayName, null, false);
         xml += parseObject(object);
-        xml += createXMLElement("laun", arrayName, null, true);
+        xml += createXMLElement(schemaNamespacePrefix, arrayName, null, true);
     }
     console.log("The translated array xml is " + xml);
     return xml;
 };
 
+/**
+ * Assembles the entire SOAP message
+ * @param xmlBody
+ * @returns {string}
+ */
 var constructXmlMessage = function (xmlBody) {
+    var soapEnvelopeHeader = "<soapenv:Envelope" +
+        " xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"" +
+        " xmlns:" + processNamespacePrefix + "=\"" + config.everteam.process_namespace + "\"" +
+        " xmlns:" + schemaNamespacePrefix + "=\"" + config.everteam.schema_namespace + "\">" +
+        "<soapenv:Header/>\n" +
+        "    <soapenv:Body>\n";
+
+    var requestStartTag = createXMLElement(processNamespacePrefix, config.everteam.process_request, null, false);
+    var requestEndTag = createXMLElement(processNamespacePrefix, config.everteam.process_request, null, true);
+
+    var soapEnvelopeFooter = "" +
+        "   </soapenv:Body>\n" +
+        "</soapenv:Envelope>";
+
     return soapEnvelopeHeader +
         requestStartTag +
         xmlBody +
@@ -73,7 +119,12 @@ var constructXmlMessage = function (xmlBody) {
 module.exports =
     {
 
-        convertJSONMessageToXMl : function (jsonMessage) {
+        /**
+         * Takes a string (assumed to be valid JSON) and attempts to convert it to a SOAP-based XML string
+         * @param jsonMessage
+         * @returns {*}
+         */
+        convertJSONMessageToXMl: function (jsonMessage) {
             var json = convertJsonStringToObject(jsonMessage);
             if (!json) return "";
 
