f036e8fee660d7e6ec6a98a2471c1d64d95feff1
April Eggers <april.eggers@flatironsjouve.com>
2017-10-25 10:20:37 -0600

    Refined the queueWatched to send ISO scores to BPMS

--- util/queueWatcher.js
+++ util/queueWatcher.js
@@ -3,6 +3,8 @@
 var stomp = require('stomp');
 var baseConfig = require('../config');
 var jsonConverter = require('./jsonConverter');
+var request = require('request');
+
 
 
 var config;
@@ -37,9 +39,9 @@ module.exports = {
         return createClient(queueType);
     },
 
-    setupEventHandlers : function (client, queueType) {
+    setupEventHandlers : function (client) {
 
-        config = baseConfig.active_mq[queueType];
+        // config = baseConfig.active_mq[queueType];
 
 
         client.on('connected', function() {
@@ -63,18 +65,68 @@ module.exports = {
         client.on('message', function(message) {
             console.log("Got message: " + message.headers['message-id']);
             console.log(JSON.stringify(message.headers));
-/*            console.log(message.body);
-            var xml = jsonConverter.convertJSONMessageToXMl(message.body[0], queueType);
+            console.log(message.body);
+            if (message.headers.destination === baseConfig.active_mq.batch.source_queue) {
+                var xml = jsonConverter.convertJSONMessageToXMl(message.body[0], "batch");
 
-            console.log("The xml being sent is " + xml);
+                console.log("The xml being sent is " + xml);
 
-            client.send({
-                'destination': config.target_queue,
-                'body': xml,
-                'persistent': 'true'
-            });*/
+                client.send({
+                    'destination': config.target_queue,
+                    'body': xml,
+                    'persistent': 'true'
+                });
 
-            client.ack(message.headers['message-id']);
+                client.ack(message.headers['message-id']);
+            } else if (message.headers.destination === baseConfig.active_mq.request.source_queue) {
+
+                var xml = jsonConverter.convertJSONMessageToXMl(message.body[0], "request");
+
+
+                // POST XML to url:config.bpm.uri + 'ode/processes/LaunchPointProcess_Processes_Core_ProcessISOCase_Process_ISO_Case_DCM',
+
+
+
+                console.log("The xml generated from the ISO request is " + xml);
+                client.ack(message.headers['message-id']);
+
+
+
+
+
+            } else if (message.headers.destination === baseConfig.active_mq.score.source_queue) {
+                console.log(JSON.stringify(message.body));
+                var xml = jsonConverter.convertJSONMessageToXMl(message.body[0], "score");
+
+                console.log("The xml generated from the ISO score is " + xml);
+
+
+                // POST XML to config.bpm.uri + 'ode/processes/LaunchPointProcess_Processes_Core_ProcessISOResponse_ISO_Response_Manager_DCM',
+
+                request({
+                    url: 'http://192.168.241.203:8080/everteam/ode/processes/LaunchPointProcess_Processes_Core_ProcessISOResponse_ISO_Response_Manager_DCM',
+                    headers: {
+                        'Content-Type': 'text/xml; charset=utf-8'
+                    },
+                    method: 'POST',
+                    body: xml
+                }, function (error, resp, data) {
+                    if (error) {
+                        console.log("Got an error " + error + " " + JSON.stringify(error));
+                        console.error(error);
+                    } else {
+                        console.log("The resp is " + resp + " " + JSON.stringify(resp));
+                        console.log("The data is " + data + " " + JSON.stringify(data));
+                    }
+                });
+
+
+                client.ack(message.headers['message-id']);
+            } else {
+                console.error("Unknown queue: " + message.headers.destination);
+            }
+
+
         });
 
         client.on('error', function(error_frame) {
