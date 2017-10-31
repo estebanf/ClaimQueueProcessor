3d55ee46670f169dd1f9ae292ddaf6b91bf7d43b
Esteban Felipe <esteban.felipe@gmail.com>
2017-10-31 19:47:14 -0400

    added env var BPMS_URI

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var baseURL = process.env.NODE_ENV === "development" ? "http://192.168.241.205:8080/" : "http://bpms.everteam.us/";
+var baseURL = process.env.BPMS_URI ||  "http://bpms.everteam.us/";
 
 var config = {};
 
